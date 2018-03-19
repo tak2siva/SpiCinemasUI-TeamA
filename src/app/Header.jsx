7512245dbd8aedb756b709d12a1ff24c94d360a2
Akshay <akshayjha92@gmail.com>
2018-03-19 11:05:36 +0530

    basic block for story #1 UI

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = () => (
   <div >
     <h1>Just Cinemas</h1>
+    <button type="button" class="btn active">NOW RUNNING</button>
+    <button type="button" class="btn">COMING SOON</button>
   </div >
 );
 
