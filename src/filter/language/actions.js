a822f42ab6636c0120b80d81bf7e9fb84c897a67
tairmansd <tairmansd@gmail.com>
2018-03-22 10:47:57 +0530

    added action test

--- src/filter/language/actions.js
+++ src/filter/language/actions.js
@@ -4,13 +4,37 @@ export const REMOVE_LANGUAGE = 'REMOVE_LANGUAGE';
 export const FETCH_LANGUAGE_PROGRESS = 'PROGRESS';
 export const FETCH_LANGUAGE_SUCCESS = 'SUCCESS';
 export const FETCH_LANGUAGE_FAILURE = 'FAILURE';
+import axios from 'axios';
 
-const addRemoveLanguages = (action, payload) => {
+export const addRemoveLanguages = (action, payload) => {
     return (dispatch, getState) => {
         dispatch({type: action, language: payload});
         fetchMovies(getState().filter.listingType, getState().filter.languages);
     }
 }
 
-export default addRemoveLanguages;
+const fetchLanguagesInProgress = {
+    type: FETCH_LANGUAGE_PROGRESS
+  }
+  
+  const languagesDataFetched = (data) => ({
+    type: FETCH_LANGUAGE_SUCCESS, 
+    payload: data,
+  });
+  
+  const languagesDataFetchFailure = {
+    type: FETCH_LANGUAGE_FAILURE,
+  };
 
+export const fetchLanguage = () => {
+    return async (dispatch) => {
+        dispatch(fetchLanguagesInProgress);
+        try {
+          const languages = await axios.get('http://localhost:9090/language');
+          dispatch(languagesDataFetched(languages.data));
+        } catch(error) {
+            console.log(error,"error----xxx")
+          dispatch(languagesDataFetchFailure)
+        }
+    }
+}
--- src/filter/language/actions.test.js
+++ src/filter/language/actions.test.js
@@ -1,26 +1,36 @@
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
 import MockAdapter from 'axios-mock-adapter';
-import addRemoveLanguages from './actions';
-import { ADD_LANGUAGE, REMOVE_LANGUAGE } from './actions'
+import applyMiddleware from 'redux';
+import {addRemoveLanguages, fetchLanguage} from './actions';
+import axios from 'axios';
+import { ADD_LANGUAGE, REMOVE_LANGUAGE, FETCH_LANGUAGE_FAILURE, FETCH_LANGUAGE_PROGRESS, FETCH_LANGUAGE_SUCCESS } from './actions';
 
-let store;
-let apiData;
-let mock;
-beforeEach(function() {
-	const middlewares = [thunk]
-	const mockStore = configureMockStore(middlewares);
-	store = mockStore({
-		filter: {
-            listingType: 'now-showing',
-			languages: []
-		}
-	});
-});
+describe("test fetch action", () => {
+    const middlewares = [thunk];
+    const mockStore = configureMockStore(middlewares);
+    const mock = new MockAdapter(axios);
+    let store;
 
-describe("add new selected languages to state", () => {
-    it('should test the action type of adding the new language', () => {
-        store.dispatch(addRemoveLanguages(ADD_LANGUAGE, "EN"));
-        expect(store.getActions()[0]).toEqual({type: ADD_LANGUAGE, language: "EN"});
-	});
+    beforeEach(() => {
+        store = mockStore({});
+    });
+
+    const mockResponse = [{"id":1,"name":"EN","description":"ENGLISH"},
+	{"id":2,"name":"HN","description":"HINDI"},
+	{"id":3,"name":"FR","description":"FRENCH"}];
+
+    it("when server responds with success", async () => {
+        
+        mock.onGet('http://localhost:9090/language').reply(200, mockResponse);
+        const expectedResponse = []
+
+        store.dispatch(fetchLanguage()).then(() => {
+			
+			const expectedActions = store.getActions();
+			expect(expectedActions.length).toBe(2);
+			expect(expectedActions).toContainEqual({"type": FETCH_LANGUAGE_PROGRESS});
+			expect(expectedActions).toContainEqual({"type": FETCH_LANGUAGE_SUCCESS, payload: mockResponse});
+		});
+    });
 });
